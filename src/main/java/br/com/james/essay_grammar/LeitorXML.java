4e91f611254ebd18865250710f83ef0fe696e1f8
Jamesasj <james@jamesalves.com.br>
2018-08-30 17:01:36 -0300

    adcionado letor de xml, xds e xml exemplo.

---
+++ src/main/java/br/com/james/essay_grammar/LeitorXML.java
@@ -0,0 +1,22 @@
+package br.com.james.essay_grammar;
+
+import java.util.List;
+
+import br.com.james.essay_grammar.models.EssayModel;
+import br.com.james.essay_grammar.models.IOFileModel;
+
+public class LeitorXML extends IOFileModel {
+
+	@Override
+	public List<EssayModel> readListEssay(String fileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EssayModel readEssay(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
