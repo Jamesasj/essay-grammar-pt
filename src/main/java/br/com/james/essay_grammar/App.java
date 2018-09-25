39792d89097ddb91dd01f6634b1e9237ffc9fc15
James Alves <james@jamesalves.com.br>
2018-09-24 21:22:15 -0300

    adicionado textos de exemplo para testes.

--- src/main/java/br/com/james/essay_grammar/App.java
+++ src/main/java/br/com/james/essay_grammar/App.java
@@ -1,5 +1,12 @@
 package br.com.james.essay_grammar;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.james.essay_grammar.models.EssayModel;
 import br.com.james.essay_grammar.models.IOFileModel;
 
@@ -9,19 +16,45 @@ public class App {
 
 	public static void main(String[] args) {
 
-		String[] arquivo = { "texto.txt" };
+		//String[] arquivo = { "dataset/texto0.txt", "dataset/texto1.txt", "dataset/texto2.txt","dataset/texto3.txt" };
+		
+		List<String> lsArquivos = lerArquivo("list_dataset.txt");
+		
 		IOFileModel fileIO = new LeitorConcreto();
 
 		fileIO.abrirArquivo("saida.csv");
 
 		fileIO.adicionarLinha(ANALISADOR.getCabecalho());
-		for (int i = 0; i < arquivo.length; i++) {
-			EssayModel redacao = fileIO.readEssay(arquivo[i]);
+		
+		for (String arquivo : lsArquivos) {
+			EssayModel redacao = fileIO.readEssay(arquivo);
 			ANALISADOR.analisarRedacao(redacao);
+			System.out.println(redacao.featuresCSV());
 			fileIO.adicionarLinha(redacao.featuresCSV());
 		}
+		
 		fileIO.fecharArquivo();
 
 		System.out.println("concluido");
+	}
+
+	private static List<String> lerArquivo(String caminho) {
+		BufferedReader br = null;
+		List<String> stList = new ArrayList<String>();
+		try {
+			br = new BufferedReader(new FileReader(caminho));
+			String linha;
+			while((linha = br.readLine())!= null) {
+				stList.add(linha);
+			}
+			br.close();
+			return stList;
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
